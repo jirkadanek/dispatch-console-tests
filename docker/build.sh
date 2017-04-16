6b5c6e743d4e185597530b89d2b8b50abf8c83fa
Jiri Danek <jdanek@redhat.com>
2017-04-16 14:08:09 +0200

    improve build.sh so that it might run on travis
    (cherry picked from commit d3c1c7f)
    
    fixup trivial error and do a set -ex to be sure next time
    
    (cherry picked from commit cf49ab2)

--- docker/build.sh
+++ docker/build.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
+set -ex
+
+# creates images jdanekrh/dispatch-console:latest and jdanekrh/dispatch-router:latest
 
 # this is to be executed manually, because of sudo
 # TODO: there is supposed to be a Docker setting that fixes file permissions...
 
+export USERNAME=`whoami`
 
-rm -rf run/console/main
-rm -rf run/router/main
+rm -rf run/console/main || true
+rm -rf run/router/main || true
 
 # The idea comes from https://www.fpcomplete.com/blog/2015/12/docker-split-images
 docker build -t build_dispatch-console --no-cache ./
 docker run --rm \
+    --volume="${HOME}/.m2:/root/.m2" \
     --volume="$PWD/run/console/main:/console" \
     --volume="$PWD/run/router/main:/router" \
     build_dispatch-console
 
-sudo chown -R jdanek run/console
-sudo chown -R jdanek run/router
+sudo chown -R ${USERNAME} run/console
+sudo chown -R ${USERNAME} run/router
 
-docker build -t dispatch-console run/console/
-docker build -t dispatch-router run/router/
+docker build -t jdanekrh/dispatch-console:latest run/console/
+docker build -t jdanekrh/dispatch-router:latest run/router/
