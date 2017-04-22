bf12c8ef2b76438d190a9e2cb676dcbabc506242
Jiri Danek <jdanek@redhat.com>
2017-04-22 12:36:59 +0200

    geckodriver errors on unknown capabilities, let's make these conditional

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -26,7 +26,7 @@ py.test -s \
  --capability browserName "${BROWSER_NAME}" \
  --capability platform "${PLATFORM}" \
  --capability version "${VERSION}" \
+ $(if [[ ${DRIVER} = "SauceLabs" ]]; then echo "--capability build travis-${TRAVIS_BUILD_NUMBER}"; fi) \
  --capability tunnel-identifier ${TRAVIS_JOB_NUMBER} \
- --capability build travis-${TRAVIS_BUILD_NUMBER} \
  --capability marionette true \
  --base-url http://127.0.0.1:8080/${CONSOLE} --verify-base-url --console ${CONSOLE}
